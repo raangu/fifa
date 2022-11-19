71f763263995144c24c317b7e72f2503f783b2a2
Raoul <raoul.erber@gmail.com>
2022-11-19 15:46:41 +0100

    list of teams

--- fifa.py
+++ fifa.py
@@ -2,13 +2,22 @@ import pandas as pd
 import streamlit as st
 #print("hello")
 
+list_teams_wc22 = 
+["Qatar", "Ecuador", "Senegal", "Netherlands",
+"England", "Iran", "USA", "Wales",
+"Argentina", "Saudi Arabia", "Mexico", "Poland",
+"France", "Australia", "Denmark", "Tunisia",
+"Spain", "Costa Rica", "Germany", "Japan",
+"Belgium", "Canada", "Morocco", "Croatia",
+"Brazil", "Serbia", "Switzerland", "Cameroon"
+"Portugal", "Ghana", "Uruguay", "South Korea"]
 
 df = pd.read_csv('wcmatches.csv')
 
 
 list_teams = list_unique_values = df['home_team'].unique()
 
-team_a = st.selectbox("Team A", list_teams)
+team_a = st.selectbox("Team A", list_teams_wc22)
 team_b = st.selectbox("Team B", list_teams)
 
 st.dataframe(df)
